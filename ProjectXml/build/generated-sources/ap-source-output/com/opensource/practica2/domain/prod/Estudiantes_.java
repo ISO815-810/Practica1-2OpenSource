99505678592c5db69d9a826c687e4dbf9d225fec
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-26 19:56:26 -0800

    test ws done

--- ProjectXml/build/generated-sources/ap-source-output/com/opensource/practica2/domain/prod/Estudiantes_.java
+++ ProjectXml/build/generated-sources/ap-source-output/com/opensource/practica2/domain/prod/Estudiantes_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-26T12:31:54")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-26T19:42:47")
 @StaticMetamodel(Estudiantes.class)
 public class Estudiantes_ { 
 
