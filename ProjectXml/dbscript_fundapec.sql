294b3c66812c558c822ac186845404d7a6722c53
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-27 09:07:17 -0800

    sql script fundapec

---
+++ ProjectXml/dbscript_fundapec.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE Fundapec;
+
+Use Fundapec;
+
+CREATE TABLE Estudiantes(
+	Id int primary key auto_increment,
+    CedulaEstudiante varchar(11) not null,
+    MatriculaEstudiante varchar(9) not null,
+    CarreraAbrev varchar(3), 
+    CreditosTotal smallint not null,
+    CreditosCursados smallint not null
+)ENGINE=InnoDb;
+
+INSERT INTO Estudiantes VALUES(1,'40215563657','20162511','ISO',237,105);
+INSERT INTO Estudiantes VALUES(2,'00104070560','20162223','ISC',247,190);
+
+SELECT * FROM Estudiantes;
