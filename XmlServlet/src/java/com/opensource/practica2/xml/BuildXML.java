28ba1ea2087d52f1caabd06e16e1837e05f35121
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-29 15:21:38 -0800

    created package xml, class BuildXML

---
+++ XmlServlet/src/java/com/opensource/practica2/xml/BuildXML.java
@@ -0,0 +1,78 @@
+package com.opensource.practica2.xml;
+
+import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
+import com.sun.org.apache.xml.internal.serialize.OutputFormat;
+import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
+import java.io.File;
+import java.io.FileWriter;
+import java.sql.ResultSet;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+/**
+ *
+ * @author DELL
+ */
+public class BuildXML {
+
+    /* printDOM will write the contents of xml document passed onto it out to a file*/
+    private static void printDOM(Document _xmlDoc, File _outputFile)
+            throws Exception {
+
+        OutputFormat outputFormat = new OutputFormat("XML", "UTF-8", true);
+
+        FileWriter fileWriter = new FileWriter(_outputFile);
+
+        XMLSerializer xmlSerializer = new XMLSerializer(fileWriter, outputFormat);
+
+        xmlSerializer.asDOMSerializer();
+
+        xmlSerializer.serialize(_xmlDoc.getDocumentElement());
+    }
+
+    /*Build XML DOcument from database. The XML object is returned to main method where it is written to flat file.*/
+    private static Document buildXML(ResultSet _studentRS)
+            throws Exception {
+
+        Document xmlDoc = new DocumentImpl();
+
+        /* Creating the root element */
+        //replace employeetable with countries to set a countries tag
+        Element rootElement = xmlDoc.createElement("Estudiantes");
+        xmlDoc.appendChild(rootElement);
+
+        while (_studentRS.next()) {
+
+            Element emp = xmlDoc.createElement("estudiante");
+            //replace employee with country for country tag
+
+            /* Build the CustomerId as a Attribute*/
+            //emp.setAttribute("empid", _studentRS.getString("empid"));
+
+            /* Creating elements within customer DOM*/
+            Element studentId = xmlDoc.createElement("id");
+            Element studentCedula = xmlDoc.createElement("cedula");
+            Element studentMatricula = xmlDoc.createElement("matricula");
+            Element studentCrdtsTotal = xmlDoc.createElement("crdtsTotal");
+            Element studentCrdtsCursados = xmlDoc.createElement("crdtsCursados");
+
+            /* Populating Customer DOM with Data*/
+            studentId.appendChild(xmlDoc.createTextNode(_studentRS.getString("id")));
+            studentCedula.appendChild(xmlDoc.createTextNode(_studentRS.getString("cedula")));
+            studentMatricula.appendChild(xmlDoc.createTextNode(_studentRS.getString("matricula")));
+            studentCrdtsTotal.appendChild(xmlDoc.createTextNode(_studentRS.getString("crdtsTotal")));
+            studentCrdtsCursados.appendChild(xmlDoc.createTextNode(_studentRS.getString("crdtsCursados")));
+
+            /* Adding the empname and role elements to the employee Element*/
+            emp.appendChild(studentId);
+            emp.appendChild(studentCedula);
+            emp.appendChild(studentMatricula);
+            emp.appendChild(studentCrdtsTotal);
+            emp.appendChild(studentCrdtsCursados);
+
+            /* Appending emp to the Root Class*/
+            rootElement.appendChild(emp);
+        }
+        return xmlDoc;
+    }
+}
--- XmlServlet/web/index.jsp
+++ XmlServlet/web/index.jsp
@@ -1,30 +1,27 @@
-<%-- 
-    Document   : index
-    Created on : 29-ene-2019, 15:08:34
-    Author     : DELL
---%>
-
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Index - Xml Generator</title>
     </head>
     <body>
         <div class="card-content">
             <form action="index.jsp" method="POST">
                 <div class="file-field input-field">
-                    <div class="btn">
-                        <span>Archivo</span>
-                        <input type="file">
-                    </div>
+
                     <div class="file-path-wrapper">
-                        <input class="file-path validate" type="text" name="file" size="50">
+                        <input class="file-path validate" type="text" name="ruta" size="50">
+                    </div>
+                    
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text" name="archivo" size="50">
                     </div>
                 </div>
+                
                 <br/>
-                <input class="btn waves-effect waves-light" name="enviar" type="submit" value="Leer Archivo">
+                
+                <input class="btn waves-effect waves-light" name="enviar" type="submit" value="">
             </form>
 
             <%
@@ -39,7 +36,7 @@
             %>        
 
             <script>
-                alert('Archivo leido correctamente.');
+                alert('Archivo Generado correctamente!!');
             </script>
 
             <%
@@ -58,7 +55,6 @@
                 }
                  */
             %>
-
         </div>
     </body>
 </html>
